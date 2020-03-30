9e198eca14cd9aca7874a6e12269c5f4d2e5deb5
Joamir Silveira <jmrsilveira@outlook.com>
2020-03-30 10:14:02 -0300

    Create database

---
+++ Class1.sql
@@ -0,0 +1,7 @@
+-- Criando a primeira tabela no banco de dados MySQL.
+create database sucos;
+-- Criando a segunda tabela com algumas definições.
+create schema sucos2 default character set utf8;
+-- Apagando as duas tabelas criadas anteriormente.
+drop database sucos;
+drop database sucos2;
