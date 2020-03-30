4a8c70c32fd2cb4f8a596bdf297a08a5aa3b7903
Joamir Silveira <jmrsilveira@outlook.com>
2020-03-30 10:13:01 -0300

    Introduction

---
+++ Class0.sql
@@ -0,0 +1,17 @@
+-- Um banco de dados serve para armazenar dados de qualquer sistema 
+
+-- Arquitetura de software MVC
+
+-- View: 
+-- Nesta camada, temos o que o usuário vê,isto é, as telas do sistema.
+-- E nela, são trabalhadas as seguintes tecnologias, por exemplo:
+-- HTML, CSS, JavaScript, Jquery
+
+-- Model:
+-- Nesta camada, encontramos as classes modelo. As classes modelos são representadas
+-- por tabelas em um banco de dados. Um banco de dados representa todos os inputs feitos
+-- pelo usuário na camada view.
+
+-- Controler:
+-- Nesta camada, são processadas as regras de negócio. Qualquer tecnologia
+-- utilizada, é processadas aqui. Exemplo: Java, C#, PHP e Ruby.
