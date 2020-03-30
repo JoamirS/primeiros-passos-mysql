cb722502972a91aa459b1110ad2604498433b9de
Joamir Silveira <jmrsilveira@outlook.com>
2020-03-30 10:17:13 -0300

    Challenge: Create a table

---
+++ Class3.sql
@@ -0,0 +1,12 @@
+-- Desafio: Nosso sistema de vendas tem mais uma tabela a ser criada: vendedores.
+-- Algumas informações:
+-- Nome da tabela deve ser TABELA_DE_VENDEDORES Vendedor tem o número interno da matrícula, 
+-- onde será armazenado no campo MATRICULA, que deve ser um string de 5 posições. O nome do vendedor deverá
+-- ser armazenado no campo NOME, e deve ser um string de 100 posições. Criar o campo PERCENTUAL_COMISSAO que representa 
+-- quantos % de comissão o vendedor ganha sobre cada venda.
+-- obs: Crie esta tabela no banco de dados SUCOS.
+
+CREATE TABLE tb_TABELA_DE_VENDEDORES
+(MATRICULA CHAR(5),
+NOME VARCHAR(100),
+PERCENTUAL_COMISSAO FLOAT);
