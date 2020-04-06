4cb168117cd45673e88912b47d338afc7bac9be1
Joamir Silveira <jmrsilveira@outlook.com>
2020-04-06 11:36:02 -0300

    Inserting two new salesperson

---
+++ Class9.sql
@@ -0,0 +1,12 @@
+-- Vamos criar mais dois vendedores, no mesmo comando SQL.
+-- Matrícula: 00235
+-- Nome: Márcio Almeida Silva
+-- Comissão: 8%
+-- ------------
+-- Matrícula: 00236
+-- Nome: Cláudia Morais 
+-- Comissão: 8%
+
+INSERT INTO tb_tabela_de_vendedores VALUES (
+'00235', 'Márcio Almeida Silva', 0.8), (
+'00236', 'Cláudia Morais', 0.8);
