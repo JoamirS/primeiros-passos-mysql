0af202ed2df699f373784baed23954befa0cc254
Joamir Silveira <jmrsilveira@outlook.com>
2020-04-11 11:44:06 -0300

    Deleting a row from the table

---
+++ Class13.sql
@@ -0,0 +1,4 @@
+-- O vendedor José Geraldo Fonseca Junior matrícula (00233) foi demitido.
+-- Retire-o da tabela de vendedores.
+USE SUCOS;
+DELETE FROM tb_tabela_de_vendedores WHERE MATRICULA = '00233';
