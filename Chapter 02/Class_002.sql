91ae8197e66d78980e254119a718c981664aff01
JoamirS <jmrsilveira@outlook.com>
2020-05-15 23:41:50 -0300

    using the truth table in MySQL

---
+++ Chapter 02/Class_002.sql
@@ -0,0 +1,7 @@
+-- Utilizando a tabela verdade, decosbrir se a expressão é verdadeira ou Falsa.   
+
+(NOT ((3 > 2) OR (4 >=5)) AND (5 > 4)) OR (9 > 0)
+(NOT VERDADEIRO OR FALSO AND VERDADEIRO) OR VERDADEIRO
+(NOT VERDADEIRO AND VERDADEIRO) OR VERDADEIRO
+FALSO OR VERDADEIRO
+VERDADEIRO
