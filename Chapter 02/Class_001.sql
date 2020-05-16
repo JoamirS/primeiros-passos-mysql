e7e7adb72a2cae1aa039b9473f0ee7657b27fdaf
JoamirS <jmrsilveira@outlook.com>
2020-05-15 23:40:16 -0300

    Recalling the previous chapter

--- Chapter 02/Class_001.sql
+++ Chapter 02/Class_001.sql
@@ -13,9 +13,10 @@ SELECT NOME_DO_PRODUTO AS PRODUTO_DO_TIPO_PET, TAMANHO, SABOR FROM tabela_de_pro
 	WHERE EMBALAGEM = 'PET';
 -- Selecionando todos os campos da tabela produtos.
 SELECT * FROM tabela_de_produtos;
+SELECT * FROM tabela_de_produtos;
 
 -- Selecionando um número do tipo float, utilizando a função between
 -- OBS: Números ponto flutuante não podem ser buscados no banco igual ao int, pois eles possuem casas a mais do que é visível no Workbench
--- Exemplo: Workbench 10.66 | Internamente: 10.87553351545
+-- Exemplo: Workbench 10.66 | Internamente: 10.6687553351545
 SELECT NOME_DO_PRODUTO AS PRODUTOS_QUE_CUSTAM_R$_10_50, EMBALAGEM, TAMANHO, SABOR, PRECO_DE_LISTA FROM tabela_de_produtos
 	WHERE PRECO_DE_LISTA BETWEEN 10.49 AND 10.52;
